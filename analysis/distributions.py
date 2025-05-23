737a0be96a12381520c30c2246c8610cc3f64d85
Jeneve Pilcher <jcpilche@iu.edu>
2025-05-23 02:21:24 -0500

    yoooo

--- analysis/distributions.py
+++ analysis/distributions.py
@@ -4,18 +4,30 @@ from pathlib import Path
 
 path = Path(__file__).resolve().parent.parent / 'data' # Go up to repo root then down to data folder
 
-#{model name: df}
+#{model: df}
 def load(folder):
     return {csv.stem: pd.read_csv(csv) for csv in folder.glob("*.csv")}
-
 product = load(path / 'product')
 business = load(path / 'business')
 
-print(isinstance(product,pd.DataFrame))
-productdf =pd.DataFrame(product)
-productnums = product.iloc[1:,1:]
 
-print(isinstance(business,pd.DataFrame))
-businessdf =pd.DataFrame(business)
-businessnums = business.iloc[1:,1:]
+
+
+product_dist = {}
+for model, df in product.items():
+    data = df.iloc[0:,1:].to_numpy()
+    product_dist[model]=data
+
+product_plot = {}
+for model, data in product_dist.items():
+    mean = np.mean(data)
+    std = np.std(data)
+    product_plot[model]=(mean,std)
+print(product_plot)
+
+
+
+#print(isinstance(business,pd.DataFrame))
+#businessdf =pd.DataFrame(business)
+#businessnums = business.iloc[1:,1:]
 
